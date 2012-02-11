7e71bda15662d6e0ec4cf1deac5a3ef9431569a0
daniel <daniel@reality.localdomain>
2012-02-10 21:51:34 -0800

    adding stumpwm configs update volume script to have correct icon/changed colors a bit

---
+++ .stump/conf/desktop.lisp
@@ -0,0 +1,129 @@
+#S(DDUMP
+   :SCREENS (#S(SDUMP
+                :NUMBER 0
+                :GROUPS (#S(GDUMP
+                            :NUMBER 1
+                            :NAME "Main"
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1440
+                                      :HEIGHT 900
+                                      :WINDOWS (20971560)
+                                      :CURRENT 20971560))
+                            :CURRENT 0)
+                         #S(GDUMP
+                            :NUMBER 2
+                            :NAME "Web"
+                            :TREE (((#S(FDUMP
+                                        :NUMBER 0
+                                        :X 0
+                                        :Y 0
+                                        :WIDTH 840
+                                        :HEIGHT 450
+                                        :WINDOWS (25165866 27262986 18874469)
+                                        :CURRENT 25165866)
+                                     #S(FDUMP
+                                        :NUMBER 2
+                                        :X 840
+                                        :Y 0
+                                        :WIDTH 600
+                                        :HEIGHT 450
+                                        :WINDOWS (18876212)
+                                        :CURRENT 18876212))
+                                    #S(FDUMP
+                                       :NUMBER 1
+                                       :X 0
+                                       :Y 450
+                                       :WIDTH 1440
+                                       :HEIGHT 450
+                                       :WINDOWS (31457289 29360137 16777225)
+                                       :CURRENT 31457289)))
+                            :CURRENT 1)
+                         #S(GDUMP
+                            :NUMBER 3
+                            :NAME "Term"
+                            :TREE (((#S(FDUMP
+                                        :NUMBER 0
+                                        :X 0
+                                        :Y 0
+                                        :WIDTH 940
+                                        :HEIGHT 450
+                                        :WINDOWS NIL
+                                        :CURRENT NIL)
+                                     #S(FDUMP
+                                        :NUMBER 1
+                                        :X 0
+                                        :Y 450
+                                        :WIDTH 940
+                                        :HEIGHT 450
+                                        :WINDOWS NIL
+                                        :CURRENT NIL))
+                                    (#S(FDUMP
+                                        :NUMBER 2
+                                        :X 940
+                                        :Y 0
+                                        :WIDTH 500
+                                        :HEIGHT 450
+                                        :WINDOWS NIL
+                                        :CURRENT NIL)
+                                     #S(FDUMP
+                                        :NUMBER 3
+                                        :X 940
+                                        :Y 450
+                                        :WIDTH 500
+                                        :HEIGHT 450
+                                        :WINDOWS NIL
+                                        :CURRENT NIL))))
+                            :CURRENT 2)
+                         #S(GDUMP
+                            :NUMBER 4
+                            :NAME "slavery"
+                            :TREE ((#S(FDUMP
+                                       :NUMBER 0
+                                       :X 0
+                                       :Y 0
+                                       :WIDTH 1440
+                                       :HEIGHT 365
+                                       :WINDOWS (33554441)
+                                       :CURRENT 33554441)
+                                    #S(FDUMP
+                                       :NUMBER 3
+                                       :X 0
+                                       :Y 365
+                                       :WIDTH 1440
+                                       :HEIGHT 355
+                                       :WINDOWS (35651593)
+                                       :CURRENT 35651593)
+                                    (#S(FDUMP
+                                        :NUMBER 1
+                                        :X 0
+                                        :Y 720
+                                        :WIDTH 720
+                                        :HEIGHT 180
+                                        :WINDOWS NIL
+                                        :CURRENT NIL)
+                                     #S(FDUMP
+                                        :NUMBER 2
+                                        :X 720
+                                        :Y 720
+                                        :WIDTH 720
+                                        :HEIGHT 180
+                                        :WINDOWS NIL
+                                        :CURRENT NIL))))
+                            :CURRENT 3)
+                         #S(GDUMP
+                            :NUMBER 5
+                            :NAME "TV"
+                            :TREE (#S(FDUMP
+                                      :NUMBER 0
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 1440
+                                      :HEIGHT 900
+                                      :WINDOWS (10485769)
+                                      :CURRENT 10485769))
+                            :CURRENT 0))
+                :CURRENT 4))
+   :CURRENT 0)
